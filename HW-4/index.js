be4d045151d7f5c0c9540e773a545cad2bba5356
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-09 21:52:52 +0300

    add

--- HW-4/index.js
+++ HW-4/index.js
@@ -1,3 +1,14 @@
-var name= prompt('What is your name?','');
-alert('Hello, '+name+'! How are you?');
-// fixed
+var number1 = parseFloat(prompt("Введите первое число"));
+var number2 = parseFloat(prompt("Введите второе число"));
+
+var sum = number1 + number2;
+var difference = number1 - number2;
+var product = number1 * number2;
+var quotient = number1 / number2;
+
+var resultString = `${number1} + ${number2} = ${sum}\n`;
+resultString += `${number1} - ${number2} = ${difference}\n`;
+resultString += `${number1} * ${number2} = ${product}\n`;
+resultString += `${number1} / ${number2} = ${quotient}`;
+
+alert(resultString);
