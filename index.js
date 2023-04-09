0e315a571da336041e0d4b674a4f0a05efc30638
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-09 22:59:00 +0300

    add-hw-5

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-var number1 = parseFloat(prompt("Введите первое число"));
-var number2 = parseFloat(prompt("Введите второе число"));
+var hours = parseFloat(prompt("Введіть кількість годин"));
 
-var sum = number1 + number2;
-var difference = number1 - number2;
-var product = number1 * number2;
-var quotient = number1 / number2;
+var seconds = hours * 3600; 
 
-var resultString = `${number1} + ${number2} = ${sum}\n`;
-resultString += `${number1} - ${number2} = ${difference}\n`;
-resultString += `${number1} * ${number2} = ${product}\n`;
-resultString += `${number1} / ${number2} = ${quotient}`;
+var resultString = `У ${hours} годинах є ${seconds} секунд`;
 
-alert(resultString);
-=======
-var name= prompt('What is your name?','');
-alert('Hello, '+name+'! How are you?');
-// fix
->>>>>>> 5c5a5dc127050f772d857fba07d40f67e21e3095
+alert(resultString); 
