2fe3130c0d960f7a727281f9d8ac5a88f26ac4ce
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-10 12:50:44 +0300

    fix

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-var number1 = parseFloat(prompt("Введите первое число"));
-var number2 = parseFloat(prompt("Введите второе число"));
+let number1 = parseFloat(prompt("Введите первое число"));
+let number2 = parseFloat(prompt("Введите второе число"));
 
-var sum = number1 + number2;
-var difference = number1 - number2;
-var product = number1 * number2;
-var quotient = number1 / number2;
+const sum = number1 + number2;
+const difference = number1 - number2;
+const product = number1 * number2;
+const quotient = number1 / number2;
 
-var resultString = `${number1} + ${number2} = ${sum}\n`;
-resultString += `${number1} - ${number2} = ${difference}\n`;
-resultString += `${number1} * ${number2} = ${product}\n`;
-resultString += `${number1} / ${number2} = ${quotient}`;
+let templateStrings = `${number1} + ${number2} = ${sum}\n`;
+templateStrings += `${number1} - ${number2} = ${difference}\n`;
+templateStrings += `${number1} * ${number2} = ${product}\n`;
+templateStrings += `${number1} / ${number2} = ${quotient}`;
 
-alert(resultString);
-=======
-var name= prompt('What is your name?','');
-alert('Hello, '+name+'! How are you?');
-// fix
->>>>>>> 5c5a5dc127050f772d857fba07d40f67e21e3095
+alert(templateStrings);
