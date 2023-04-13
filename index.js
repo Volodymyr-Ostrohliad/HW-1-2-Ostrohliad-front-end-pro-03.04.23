3697cbfac2a92c1bb18e522e700d7c0613ac7153
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-13 19:12:36 +0300

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-let a = Number(prompt("Введите первое число"));
-let b = Number(prompt("Введите второе число"));
-let c = Number(prompt("Введите третье число"));
-let d = Number(3)
-console.log(a,b,c);
+let a = (prompt("Введите первое число"));
+let b = (prompt("Введите второе число"));
+let c = (prompt("Введите третье число"));
+let d = (3)
+console.log(a, b, c);
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
   alert("Ошибка! Введите только числа.");
 } else {
   const sum = (a + b + c) / d;
-  console.log(sum);}
-
-const sum = (a + b + c) / d;
-console.log(sum)
-
-let templateStrings = `${a} + ${b} + ${c} / ${d} = ${sum.toFixed(1)}`;
-alert(templateStrings)
+  console.log(sum);
+  let templateStrings = `${a} + ${b} + ${c} / ${d} = ${sum.toFixed(1)}`;
+  alert(templateStrings);
+}
