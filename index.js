6e52cec928a4759e51d4d37e7f935d75944e8d2c
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-13 19:20:02 +0300

    fixed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let a = prompt("Введите первое число");
 let b = prompt("Введите второе число");
 let c = prompt("Введите третье число");
-let d = (3)
+let d = 3;
 console.log(a, b, c);
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
   alert("Ошибка! Введите только числа.");
 } else {
+  a = parseFloat(a);
+  b = parseFloat(b); 
+  c = parseFloat(c); 
   const sum = (a + b + c) / d;
   console.log(sum);
   let templateStrings = `${a} + ${b} + ${c} / ${d} = ${sum.toFixed(1)}`;
