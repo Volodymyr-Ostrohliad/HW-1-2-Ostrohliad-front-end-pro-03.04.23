9c4e68b2c2b7a0c751b36357791bbeb7944088c6
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-04-13 20:20:19 +0300

    a

--- index.js
+++
@@ -1,7 +0,0 @@
-let hours = parseFloat(prompt("Введіть кількість годин"));
-
-let seconds = hours * 3600; 
-
-let templateStrings = `У ${hours} годинах є ${seconds} секунд`;
-
-alert(templateStrings); 
